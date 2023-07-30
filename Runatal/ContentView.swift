19475ef8c401c337c00be980c1e40f83162bb667
Andrew Skurlatov <a.skurlatov@gmail.com>
2023-07-30 17:53:01 +0200

    Initial Commit

---
+++ Runatal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Runatal
+//
+//  Created by Andrey Skurlatov on 30.7.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Runatal/RunatalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunatalApp.swift
+//  Runatal
+//
+//  Created by Andrey Skurlatov on 30.7.23..
+//
+
+import SwiftUI
+
+@main
+struct RunatalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
