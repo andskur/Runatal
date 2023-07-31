d22b19086b1efabf172567a3bfc5528a113116fb
Andrew Skurlatov <a.skurlatov@gmail.com>
2023-07-31 11:09:37 +0200

    📦 chore(main.yml): add GitHub Actions workflow for running tests and releasing 🔧 fix(project.pbxproj): add necessary files and configurations for unit tests
    🔧 fix(Runatal.xcscheme): remove shouldAutocreateTestPlan attribute
    📝 chore(Runatal.xcscheme): add TestPlanReference for Runatal.xctestplan
    📝 chore(Runatal.xctestplan): add configuration and test target information
    📝 chore(RunesControllerTests.swift): add unit test for loading runes data

---
+++ Tests/UnitTests/RunesControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  RunatalTests.swift
+//  RunatalTests
+//
+//  Created by Andrey Skurlatov on 31.7.23..
+//
+
+import XCTest
+@testable import Runatal
+
+final class RunatalTests: XCTestCase {
+    var runesController: RunesController!
+    
+    override func setUp() {
+        super.setUp()
+        runesController = RunesController()
+    }
+
+    override func tearDown() {
+        runesController = nil
+        super.tearDown()
+    }
+
+    func testLoadRunesData() {
+        runesController.loadRunesData()
+        XCTAssertFalse(runesController.loadedRunes.isEmpty)
+    }
+}
