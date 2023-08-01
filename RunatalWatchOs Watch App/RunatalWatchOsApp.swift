99ad2c08eb459c6bbf908a80ab738bafa0bacbf9
Andrew Skurlatov <a.skurlatov@gmail.com>
2023-08-01 16:46:22 +0200

    🔨 chore(project.pbxproj): add RunatalWatchOs Watch App files to project 🔧 fix(project.pbxproj): fix build phases for RunatalWatchOs Watch App target
    🔧 chore(project.pbxproj): add Debug and Release build configurations for RunatalWatchOs Watch App target
    
    📝 chore(xcodeproj): update xcschememanagement.plist
    📝 feat(xcodeproj): add new files for Watch App
    📝 feat(xcodeproj): add new files for AppIcon
    📝 feat(xcodeproj): add new file for AccentColor
    📝 feat(xcodeproj): add new file for Contents.json
    📝 feat(xcodeproj): add new file for RunatalWatchOsApp.swift
    📝 feat(xcodeproj): add new file for RunesListView.swift
    📝 feat(xcodeproj): add new file for RunatalWatchOs_Watch_AppTests.swift
    📝 feat(xcodeproj): add new file for RunatalWatchOs_Watch_AppUITests.swift
    📝 feat(xcodeproj): add new file for RunatalWatchOs_Watch_AppUITestsLaunchTests.swift

---
+++ RunatalWatchOs Watch App/RunatalWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunatalWatchOsApp.swift
+//  RunatalWatchOs Watch App
+//
+//  Created by Andrey Skurlatov on 1.8.23..
+//
+
+import SwiftUI
+
+@main
+struct RunatalWatchOs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RunesListView()
+        }
+    }
+}
---
+++ RunatalWatchOs Watch App/RunesListView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  RunatalWatchOs Watch App
+//
+//  Created by Andrey Skurlatov on 1.8.23..
+//
+
+import SwiftUI
+
+struct RunesListView: View {
+    @StateObject private var controller = RunesController()
+    @State private var isSettingsActive = false
+    
+    var settings: some View {
+        Group {
+            Form {
+                Picker("Translation", selection: $controller.translation) {
+                    ForEach(TranslationLanguage.allCases, id: \.self) { translation in
+                        Text(translation.rawValue.capitalized).tag(translation)
+                    }
+                }
+            }
+            .padding()
+            
+         }
+        .opacity(0.8)
+        .padding(10)
+    }
+    
+    var body: some View {
+        ScrollViewReader {value in
+            ScrollView {
+                Button(action: {isSettingsActive.toggle() }, label: {
+                    Text("Settings")
+                    Image(systemName: "gear")
+                })
+                .fullScreenCover(isPresented: $isSettingsActive) {
+                    settings
+                }
+                
+                Spacer()
+                Spacer()
+                
+                LazyVStack(alignment: .leading) { // Use LazyVStack for improved performance
+                    ForEach(controller.loadedRunes) { rune in
+                        Group {
+                            Text(rune.Symbol).font(.system(size: 90)).fontWeight(.bold)
+                            Divider()
+                            Group {
+                                Text(rune.Name).font(.system(size: 30))
+                                Text(rune.Meaning.generate(language: controller.translation).capitalized)
+                            }
+                            Divider()
+                            Text("Sound: \(rune.Sound)")
+                            Divider()
+                        }
+                        .padding()
+                    }
+                    .navigationTitle("Words")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RunesListView()
+    }
+}
